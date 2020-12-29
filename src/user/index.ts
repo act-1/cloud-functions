b9a3fc559632b1e0b36aa8b61da4e32dd21cef99
Guy Tepper <guytepper@gmail.com>
2020-12-29 21:09:28 +0200

    User: Create firestore doc upon new user creation

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,6 +1,30 @@
 import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
 
+export const onUserCreation = functions.auth.user().onCreate(async (user) => {
+  const isAnonymous = user.providerData.length === 0;
+
+  try {
+    if (isAnonymous) {
+      const userRef = firestore().collection('users').doc(user.uid);
+      const userDoc = await userRef.get();
+
+      if (!userDoc.exists) {
+        const result = await userRef.set({
+          isAnonymous: true,
+          createdAt: firestore.FieldValue.serverTimestamp(),
+        });
+
+        return result;
+      }
+      throw 'User already exists.';
+    }
+    throw 'Only anonymous users are currently supported.';
+  } catch (err) {
+    throw new functions.https.HttpsError('not-found', err.message);
+  }
+});
+
 export const createUserFCMToken = functions.https.onCall(async (data, context) => {
   if (!context.auth) throw new functions.https.HttpsError('unauthenticated', 'Not authenticated.');
   if (!data.fcmToken) throw new functions.https.HttpsError('invalid-argument', 'Missing token.');

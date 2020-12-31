6e5d78744698301f415d7d6d02b3ba45ebb8c225
Guy Tepper <guytepper@gmail.com>
2020-12-31 10:45:25 +0200

    Database url config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as admin from 'firebase-admin';
 
-admin.initializeApp({ credential: admin.credential.applicationDefault(), projectId: 'act1co' });
+let databaseURL = 'https://act1co-default-rtdb.firebaseio.com/';
+
+if (process.env.FUNCTIONS_EMULATOR) {
+  databaseURL = 'http://localhost:9000/?ns=act1co';
+}
+
+admin.initializeApp({ credential: admin.credential.applicationDefault(), projectId: 'act1co', databaseURL });
 
 export * from './user';
 export * from './events';

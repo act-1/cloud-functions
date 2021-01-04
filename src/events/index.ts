5b63d67a9a063970c40f22da6ac55c1b616b612e
Guy Tepper <guytepper@gmail.com>
2021-01-04 17:58:42 +0200

    Scheduale updatePastEvents function

--- src/events/index.ts
+++ src/events/index.ts
@@ -74,3 +74,23 @@ export const unattendEvent = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError('not-found', err.message);
   }
 });
+
+/**
+ * The function fetches all past events which haven't been marked as "pastEvent", and mark them correctly.
+ */
+exports.updatePastEvents = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
+  try {
+    const pastEventsSnapshot = await firestore()
+      .collection('events')
+      .where('pastEvent', '==', false)
+      .where('endDate', '<=', new Date())
+      .get();
+
+    const updates = pastEventsSnapshot.docs.map((event) => event.ref.set({ pastEvent: true }));
+    await Promise.all(updates);
+
+    return true;
+  } catch (err) {
+    throw new functions.https.HttpsError('not-found', err.message);
+  }
+});

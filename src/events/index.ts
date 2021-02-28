fd6f8162f2088d52ac9711a55f067bb1ba9964dc
Guy Tepper <guytepper@gmail.com>
2021-02-28 16:15:56 +0200

    Update past events status

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,17 +2,17 @@ import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
 
 /**
- * The function fetches all past events which haven't been marked as "pastEvent", and mark them correctly.
+ * The function fetches all past events and set their status correctly.
  */
 exports.updatePastEvents = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
   try {
     const pastEventsSnapshot = await firestore()
       .collection('events')
-      .where('pastEvent', '==', false)
-      .where('endDate', '<=', new Date())
+      .where('status', '==', 'upcoming')
+      .where('endDate', '<', new Date())
       .get();
 
-    const updates = pastEventsSnapshot.docs.map((event) => event.ref.update({ pastEvent: true }));
+    const updates = pastEventsSnapshot.docs.map((event) => event.ref.update({ status: 'past' }));
     await Promise.all(updates);
 
     return true;

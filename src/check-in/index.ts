5fd48b0a87edb61c02af36dca5be3f3ee7ef1032
Guy Tepper <guytepper@gmail.com>
2021-03-16 13:05:09 +0200

    Update index.ts

--- src/check-in/index.ts
+++ src/check-in/index.ts
@@ -5,32 +5,47 @@ exports.onCheckIn = functions.firestore.document('checkIns/{docId}').onCreate(as
   try {
     const checkInData = snap.data();
 
-    const { id: checkInId, locationId, userId, displayName, privacySetting, eventId } = checkInData;
+    const { region } = checkInData;
 
     // Update stats.
-    await database().ref('locationCounter').child(locationId).set(database.ServerValue.increment(1));
-    await database().ref('currentCheckIns').set(database.ServerValue.increment(1));
 
-    /**
-     * Privacy settings:
-     * PUBLIC - Add a check in entry to the location's RTDB ref and to the user's firestore document.
-     * PRIVATE - Add the check in entry to the user's firestore document.
-     * ANONYMOUS - Don't do anything, only increment the stats (above).
-     */
+    await database().ref('regionCounter').child(region).set(database.ServerValue.increment(1));
+    await database().ref('totalCounter').set(database.ServerValue.increment(1));
 
-    if (privacySetting === 'PUBLIC') {
-      await database().ref(`checkIns/${locationId}/${checkInId}`).set({
-        createdAt: database.ServerValue.TIMESTAMP,
-        isActive: true,
-        profilePicture: checkInData.profilePicture,
-      });
+    // Log for future stats
+    await firestore().collection('checkInLogger').add({ region, createdAt: firestore.FieldValue.serverTimestamp() });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+});
 
-      if (eventId) {
-        // Add user to the chat room.
-        // Only allowed for events; not available for location check ins.
-        await database().ref(`chat/rooms/${eventId}/members/${userId}`).set({ displayName });
-      }
+exports.onCheckInUpdate = functions.firestore.document('checkIns/{docId}').onUpdate(async (change) => {
+  try {
+    const before = change.before.data();
+    const after = change.after.data();
+
+    if (before.region !== after.region) {
+      await database().ref('regionCounter').child(before.region).set(database.ServerValue.increment(-1));
+      await database().ref('regionCounter').child(after.region).set(database.ServerValue.increment(1));
+
+      // Log for future stats
+      await firestore()
+        .collection('checkInLogger')
+        .add({ region: after.region, createdAt: firestore.FieldValue.serverTimestamp() });
     }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+});
+
+exports.onCheckInDelete = functions.firestore.document('checkIns/{docId}').onDelete(async (snap) => {
+  try {
+    const checkInData = snap.data();
+
+    await database().ref('regionCounter').child(checkInData.region).set(database.ServerValue.increment(-1));
+    await database().ref('regionCounter').set(database.ServerValue.increment(-1));
   } catch (err) {
     console.error(err);
     throw err;
@@ -40,103 +55,103 @@ exports.onCheckIn = functions.firestore.document('checkIns/{docId}').onCreate(as
 /**
  * Retrieves all expired check ins, updates their isActive property and decrements their location.
  */
-async function updateCheckInCount() {
-  try {
-    const expiredCheckInsSnapshot = await firestore()
-      .collection('checkIns')
-      .where('expireAt', '<=', new Date())
-      .where('isActive', '==', true)
-      .get();
+// async function updateCheckInCount() {
+//   try {
+//     const expiredCheckInsSnapshot = await firestore()
+//       .collection('checkIns')
+//       .where('expireAt', '<=', new Date())
+//       .where('isActive', '==', true)
+//       .get();
 
-    let checkInLocations = {};
+//     let checkInLocations = {};
 
-    // Create an object which contains the locationIds and their respective check ins.
-    expiredCheckInsSnapshot.docs.forEach((checkIn) => {
-      const { locationId, userId } = checkIn.data();
-      if (!checkInLocations[locationId]) {
-        checkInLocations[locationId] = [{ checkInId: checkIn.id, userId }];
-      } else {
-        checkInLocations[locationId].push({ checkInId: checkIn.id, userId });
-      }
-    });
+//     // Create an object which contains the locationIds and their respective check ins.
+//     expiredCheckInsSnapshot.docs.forEach((checkIn) => {
+//       const { locationId, userId } = checkIn.data();
+//       if (!checkInLocations[locationId]) {
+//         checkInLocations[locationId] = [{ checkInId: checkIn.id, userId }];
+//       } else {
+//         checkInLocations[locationId].push({ checkInId: checkIn.id, userId });
+//       }
+//     });
 
-    const locationIds = Object.keys(checkInLocations);
+//     const locationIds = Object.keys(checkInLocations);
 
-    const updateRequests = locationIds.map(async (locationId) => {
-      let expiredLocationCheckInCounter = 0;
-      const locationCheckIns = checkInLocations[locationId];
+//     const updateRequests = locationIds.map(async (locationId) => {
+//       let expiredLocationCheckInCounter = 0;
+//       const locationCheckIns = checkInLocations[locationId];
 
-      try {
-        // Take all check in for a location.
+//       try {
+//         // Take all check in for a location.
 
-        const batch = firestore().batch();
+//         const batch = firestore().batch();
 
-        // Set isActive check in flag to false.
-        const checkInPromises = await locationCheckIns.map(async (checkIn) => {
-          const { checkInId, userId } = checkIn;
+//         // Set isActive check in flag to false.
+//         const checkInPromises = await locationCheckIns.map(async (checkIn) => {
+//           const { checkInId, userId } = checkIn;
 
-          const checkInRef = firestore().collection(`checkIns`).doc(checkInId);
+//           const checkInRef = firestore().collection(`checkIns`).doc(checkInId);
 
-          batch.update(checkInRef, { isActive: false, updatedAt: firestore.FieldValue.serverTimestamp() });
+//           batch.update(checkInRef, { isActive: false, updatedAt: firestore.FieldValue.serverTimestamp() });
 
-          const publicCheckInRef = database().ref(`checkIns/${locationId}/${checkInId}`);
+//           const publicCheckInRef = database().ref(`checkIns/${locationId}/${checkInId}`);
 
-          try {
-            const publicCheckInSnapshot = await publicCheckInRef.once('value');
+//           try {
+//             const publicCheckInSnapshot = await publicCheckInRef.once('value');
 
-            if (publicCheckInSnapshot.exists()) {
-              await publicCheckInRef.update({ isActive: false, updatedAt: database.ServerValue.TIMESTAMP });
-            }
-          } catch (err) {
-            console.error(err);
-          }
+//             if (publicCheckInSnapshot.exists()) {
+//               await publicCheckInRef.update({ isActive: false, updatedAt: database.ServerValue.TIMESTAMP });
+//             }
+//           } catch (err) {
+//             console.error(err);
+//           }
 
-          expiredLocationCheckInCounter++;
-          console.log(`Check in activity ${checkInId} was set to off (pending batch commit).`);
-        });
+//           expiredLocationCheckInCounter++;
+//           console.log(`Check in activity ${checkInId} was set to off (pending batch commit).`);
+//         });
 
-        // Wait for all check ins batch additions to finish
-        await Promise.all(checkInPromises);
+//         // Wait for all check ins batch additions to finish
+//         await Promise.all(checkInPromises);
 
-        // Commit check in firestore updates
-        await batch.commit();
+//         // Commit check in firestore updates
+//         await batch.commit();
 
-        // Decrement expired check ins from location counter.
-        const decrementCount = -expiredLocationCheckInCounter;
-        await database().ref('locationCounter').child(locationId).set(database.ServerValue.increment(decrementCount));
+//         // Decrement expired check ins from location counter.
+//         const decrementCount = -expiredLocationCheckInCounter;
+//         await database().ref('locationCounter').child(locationId).set(database.ServerValue.increment(decrementCount));
 
-        console.log(`Removed ${expiredLocationCheckInCounter} check ins from ${locationId}`);
+//         console.log(`Removed ${expiredLocationCheckInCounter} check ins from ${locationId}`);
 
-        return true;
-      } catch (err) {
-        console.error(err);
-        throw err;
-      }
-    });
-    await Promise.all(updateRequests);
-    await database().ref('currentCheckIns').set(database.ServerValue.increment(-expiredCheckInsSnapshot.docs.length));
-    console.log('Total check ins removed: ', expiredCheckInsSnapshot.docs.length);
-  } catch (err) {
-    throw err;
-  }
-}
+//         return true;
+//       } catch (err) {
+//         console.error(err);
+//         throw err;
+//       }
+//     });
+//     await Promise.all(updateRequests);
+//     await database().ref('currentCheckIns').set(database.ServerValue.increment(-expiredCheckInsSnapshot.docs.length));
+//     console.log('Total check ins removed: ', expiredCheckInsSnapshot.docs.length);
+//   } catch (err) {
+//     throw err;
+//   }
+// }
 
-exports.updateCheckInCountManually = functions.https.onCall(async (data, context) => {
-  try {
-    if (!context.auth) throw new functions.https.HttpsError('unauthenticated', 'Not authenticated.');
+// exports.updateCheckInCountManually = functions.https.onCall(async (data, context) => {
+//   try {
+//     if (!context.auth) throw new functions.https.HttpsError('unauthenticated', 'Not authenticated.');
 
-    const result = await updateCheckInCount();
-    return result;
-  } catch (err) {
-    throw err;
-  }
-});
+//     const result = await updateCheckInCount();
+//     return result;
+//   } catch (err) {
+//     throw err;
+//   }
+// });
 
-exports.updateCheckInCount = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
-  try {
-    await updateCheckInCount();
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-});
+// exports.updateCheckInCount = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
+//   try {
+//     await updateCheckInCount();
+//   } catch (err) {
+//     console.error(err);
+//     throw err;
+//   }
+// });
